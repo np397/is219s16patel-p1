dd5e11f3660197f8e9b3931def8f8ede62f25431
np397 <nidhipatel062194@gmail.com>
2016-02-19 21:25:29 -0500

    Changed the columns for the profile section

--- index.php
+++ index.php
@@ -122,11 +122,11 @@
 			<h2>Personal Profile</h2>
 			<hr>
 			<div class="row">
-				<div class="col-sm-4 col-sm-offset-2">
+				<div class="col-sm-2">
 					<img src="img7.jpg" class="img-rounded">
 				</div>
-				<div class="col-sm-7">
-					<br><br>
+				<div class="col-sm-10">
+					<br>
 					<p>Hello, and welcome! My name is Nidhi Patel and I am a junior at <a href="http://www.njit.edu/">NJIT</a>.
 					   I am currently pursuing a Bachelor's of Science Degree in Information Technology with a concentration 
 					   in web development. As a self motivated person, I enjoy living a goal oriented life. Challenges and opportunities 
@@ -134,7 +134,6 @@
 					   starter with ability to work independently and enthusiasm to work with a team in a 
 					   fast-paced environment. 
 					</p>
-					<br>
 					<p>I enjoy going on adventurous trips with my friends and family. Going on hikes is 
 					   my favourite stress relieving activity.Other than travelling and hiking, I like bowling, playing 
 					   Badminton, cooking and coding.</p>
